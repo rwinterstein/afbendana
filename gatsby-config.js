31a78365d6f85a198d6ecbe7036e6dd4914e202b
Robert Winterstein <rrwinterstein@gmail.com>
2021-09-10 17:57:21 -0400

    updates config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,18 +15,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Screenwriter`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        // icon: `src/images/`, // This path is relative to the root of the site.
-      },
-    },
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-styled-components`,
